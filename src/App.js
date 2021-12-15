52500d8475dc4c098b2a384c22c1bc60f91d02fc
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-12-14 16:49:01 -0800

    3 card spread functions, images not loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './App.scss';
 import {Container, Row, Col } from 'react-bootstrap';
-import cards from './data.json';
 import { TarotCard } from './components/TarotCard';
 import { Confirmation } from './components/Confirmation';
+import cards from './data';
+
+
 
 function App() {
+  const [ordered, setOrdered] = useState(false);
+
+  function displayConfirmation() {
+    setOrdered(true);
+
+    setTimeout(() => {
+      setOrdered(false);
+    }, 3000);
+  }
   return (
     <Container>
+      {ordered && <Confirmation toggle = {setOrdered} />}
       <Row>
-        {cards}
+        {cards.map(data => (
+          <Col xs={3} className="mb-5" key={`$data.id`}>
+            <TarotCard data={data} setOrdered={displayConfirmation} />
+          </Col>
+        ) )}
       </Row>
       </Container>
   );
--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -3,6 +3,14 @@ import { Toast } from 'react-bootstrap';
 
 export function Confirmation ({ toggle }) {
   return (
-    <Toast onClose = {() => toggle(false)}
+    <Toast onClose = {() => toggle(false)}>
+      <Toast.Header>
+        <strong className="mr-auto">Your Fate is Uncertain</strong>
+        <small>text</small>
+      </Toast.Header>
+      <Toast.Body>
+        Your unavoidable doom approaches
+      </Toast.Body>
+    </Toast>
   )
 }
